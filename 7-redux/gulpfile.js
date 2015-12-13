a7053a25a1692a4c6040191296f759745aa59ced
Ruby Boyarski <rubyboy@gmail.com>
2015-12-13 10:49:04 +0200

    Updated gulp

--- 7-redux/gulpfile.js
+++ 7-redux/gulpfile.js
@@ -20,15 +20,25 @@ gulp.task('clean', function() {
     return gulp.src(paths.dist, {read: false}).pipe(clean());
 });
 
-
-gulp.task('package',
-    shell.task(['jspm bundle-sfx app/main.ts ' + paths.dist + "/" + paths.targetJS])
-);
-
+// copy required sources to the dist folder
 gulp.task('copy', function(){
     gulp.src(paths.sourcesToCopy).pipe(gulp.dest(paths.dist));
 });
 
+// bundle the app with jspm
+gulp.task('bundle',
+    shell.task(['jspm bundle-sfx app/main.ts ' + paths.dist + "/" + paths.targetJS])
+);
+
+// minify the bundle
+gulp.task('minify', function() {
+    gulp.src(paths.targetJS, {cwd: paths.dist})
+        .pipe(uglify())
+        .pipe(concat('index.min.js'))
+        .pipe(gulp.dest(paths.dist));
+});
+
+// update index.html to point to the minified bundle
 gulp.task('update-target-html', function(){
     gulp.src([paths.targetHTML])
         // remove scripts
@@ -42,20 +52,14 @@ gulp.task('update-target-html', function(){
         .pipe(gulp.dest(paths.dist))
 });
 
-gulp.task('minify', function() {
-    gulp.src(paths.targetJS, {cwd: paths.dist})
-        .pipe(uglify())
-        .pipe(concat('index.min.js'))
-        .pipe(gulp.dest(paths.dist));
-});
-
+// entry point - run tasks in a sequence
 gulp.task('default', function(callback) {
     runSequence(
         'clean',
         'copy',
-        'package',
-        'update-target-html',
+        'bundle',
         'minify',
+        'update-target-html',
         function (error) {
             if (error) {
                 console.log(error.message);
