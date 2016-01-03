4942c4217eb5012e1f252bc4b69eef13c9940bf4
Ruby Boyarski <rboyarski@infomedia.com.au>
2016-01-03 12:06:31 +0200

    Updating typings task

--- 7-redux/tasks/typings.js
+++ 7-redux/tasks/typings.js
@@ -1,26 +1,29 @@
-var fs = require("fs");
-var typingsDependencies = JSON.parse(fs.readFileSync("package.json")).typingsDependencies;
+var gulp = require('gulp');
+gulp.task("typings", function() {
+  var fs = require("fs");
+  var typingsDependencies = JSON.parse(fs.readFileSync("package.json")).typingsDependencies;
 
-var result = {"devDependencies": {},"ambientDevDependencies": {}};
+  var result = {"devDependencies": {},"ambientDevDependencies": {}};
 
-// generate dt dependencies
-typingsDependencies && (typingsDependencies.registry||[]).forEach(value => {
-    result.ambientDevDependencies[value] = "github:DefinitelyTyped/DefinitelyTyped/" + value + "/" + value + ".d.ts";
+  // generate dt dependencies
+  typingsDependencies && (typingsDependencies.registry||[]).forEach(value => {
+      result.ambientDevDependencies[value] = "github:DefinitelyTyped/DefinitelyTyped/" + value + "/" + value + ".d.ts";
+  });
+
+  // generate local d.ts dependencies
+  typingsDependencies && (typingsDependencies.file||[]).forEach((path) => {
+      var dependencyName = path.replace(/@VERSION.*/, "");
+      var parentPath = dependencyName.replace(/[^\/]*$/, "");
+      var namePrefix = dependencyName.replace(parentPath,"");
+      var matchingFolders = fs.readdirSync(parentPath)
+                              .filter(name => name.startsWith(namePrefix+"@"))
+                              .filter(name => fs.lstatSync(parentPath + "/" + name).isDirectory());
+      if (matchingFolders.length > 0) {
+          result.devDependencies[namePrefix] = "file:" + path.replace(/VERSION/, matchingFolders[0].replace(/.*@/,""));
+      } else {
+          console.log("Couldn't find a single match for '" + path + "' in " + parentPath);
+      }
+  });
+
+  fs.writeFile("typings.json", JSON.stringify(result, null, 4), err => console.log(err||"Created typings.json"));
 });
-
-// generate local d.ts dependencies
-typingsDependencies && (typingsDependencies.file||[]).forEach((path) => {
-    var dependencyName = path.replace(/@VERSION.*/, "");
-    var parentPath = dependencyName.replace(/[^\/]*$/, "");
-    var namePrefix = dependencyName.replace(parentPath,"");
-    var matchingFolders = fs.readdirSync(parentPath)
-                            .filter(name => name.startsWith(namePrefix+"@"))
-                            .filter(name => fs.lstatSync(parentPath + "/" + name).isDirectory());
-    if (matchingFolders.length > 0) {
-        result.devDependencies[namePrefix] = "file:" + path.replace(/VERSION/, matchingFolders[0].replace(/.*@/,""));
-    } else {
-        console.log("Couldn't find a single match for '" + path + "' in " + parentPath);
-    }
-});
-
-fs.writeFile("typings.json", JSON.stringify(result, null, 4), err => console.log(err||"Created typings.json"));
