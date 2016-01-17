f6abc1b219f67713fff4614aa016fdde30674e9f
Ruby Boyarski <rboyarski@infomedia.com.au>
2016-01-17 16:25:40 +0200

    Added property and event bindings

--- 1-getting-started/app/app-component.ts
+++ 1-getting-started/app/app-component.ts
@@ -1,9 +1,19 @@
 import {Component} from 'angular2/core'
 
 @Component({
-    selector: 'my-app',
-    template: `<h3>{{title}}</h3>`
+    selector: "my-app",
+    template: `<h3 [style.color]="color" (click)="updateColor()">{{title}}</h3>`
 })
 export class AppComponent {
-    title:string = "Angular 2.0 is in the house.";
+    private title:string = "Angular 2.0 is in the house.";
+    private color:string;
+  	constructor() {
+  		this.updateColor();
+  	}
+    getColor() {
+      return "#"+((1<<24)*Math.random()|0).toString(16);
+    }
+    updateColor() {
+      this.color = this.getColor();
+    }
 }
