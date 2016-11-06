4432c453c3301b2193b7460638b6019293a18898
Ruby Boyarski <rboyarski@infomedia.com.au>
2016-11-06 12:30:24 +0200

    Cleanup

--- 1-getting-started/app/main.ts
+++ 1-getting-started/app/main.ts
@@ -1,7 +1,7 @@
 import 'zone.js'
 import 'reflect-metadata'
 
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'
-import { AppModule } from './app.module'
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic'
+import {AppModule} from './app.module'
 
 platformBrowserDynamic().bootstrapModule(AppModule);
